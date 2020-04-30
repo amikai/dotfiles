daa1b02f5ce4c06c1a6eab6170ecc4d84cd4c3e5
莊雲凱 <amikai@zhuangyunkais-MacBook-Pro.local>
2020-04-30 12:22:30 +0800

    Basic setting
    Inspired by https://github.com/mathiasbynens/dotfiles

---
+++ brew.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+# Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/bash";
+fi;
+
+#
+brew install wget
+
+brew install curl
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim
+brew install grep
+brew install openssh
+brew install screen
+brew install php
+brew install gmp
+
+# Install other useful binaries.
+brew install ack
+#brew install exiv2
+brew install git
+brew install git-lfs
+brew install gs
+brew install imagemagick
+brew install lua
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rlwrap
+brew install ssh-copy-id
+brew install tree
+brew install vbindiff
+brew install zopfli
+
+# ----- My personal tool -----
+brew install tmux
+brew install neovim
+brew install the_silver_searcher
+brew install ripgrep
+brew isntall python
+brew install pyenv
+brew install pyenv-virtualenv
+
+brew install homebrew/cask/google-chrome
+brew install homebrew/cask/visual-studio-code
+brew install homebrew/cask/iterm2
+brew install homebrew/cask/telegram
+
+# Remove outdated versions from the cellar.
+brew cleanup
