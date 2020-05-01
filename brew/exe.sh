4c2aa2710c5e7496167c55a408ec15b0ff34887e
莊雲凱 <amikai@zhuangyunkais-MacBook-Pro.local>
2020-05-01 08:48:02 +0800

    All homebrew installation script
    basic.sh: Commonly used tools in the system
    extra.sh: Personally used tools
    python.sh: Python enviroment setup
    exe.sh: Execute basic.sh extra.sh and others installation script

---
+++ brew/exe.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for file in ./{basic.sh,extra.sh}; do
+	[ -r "$file" ] && [ -f "$file" ] && bash "$file";
+done;
+unset file;
+
+for file in ./{python.sh}; do
+	[ -r "$file" ] && [ -f "$file" ] && bash "$file";
+done;
+unset file;
---
+++ brew/python.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# install python2
+pyenv install-latest 2.7
+
+# install python3
+pyenv install-latest
+
+pyenv shell 2.7 3
+
