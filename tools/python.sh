7fa791bc486cb5a67648c158d39285ef1c8ddc4c
amikai <as23041248@gmail.com>
2020-05-02 15:02:19 +0800

    Pyenv install the lastest version of python2 and python3

--- tools/python.sh
+++ tools/python.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+PYENV_LASTEST_VERSION3=$(pyenv install --list | tr -d ' ' | grep '^3.[0-9].[0-9]$' | tail -1)
+
+PYENV_LASTEST_VERSION2=$(pyenv install --list | tr -d ' ' | grep '^2.[0-9].[0-9]$' | tail -1)
+
+# init pyenv
+eval "$(pyenv init -)"
+
+# init venv
+eval "$(pyenv virtualenv-init -)"
+
 # install python2
-pyenv install-latest 2.7
+pyenv install "$PYENV_LASTEST_VERSION2"
 
 # install python3
-pyenv install-latest
+pyenv shell "$PYENV_LASTEST_VERSION2" "$PYENV_LASTEST_VERSION3"
 
-pyenv shell 2.7 3
-
+unset PYENV_LASTEST_VERSION2 PYENV_LASTEST_VERSION3
