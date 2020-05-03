eb7520936be8651a6701b04ae71ed696c94ba0b0
amikai <as23041248@gmail.com>
2020-05-03 23:20:19 +0800

    Install apps

--- apps/basic.sh
+++ apps/basic.sh
@@ -6,6 +6,7 @@ brew tap homebrew/cask-versions
 
 # Mac App Store command line
 brew install mas
+mas install 497799835  # Xcode (11.4.1)
 
 brew cask install font-fira-code
 brew cask install google-chrome
--- apps/extra.sh
+++ apps/extra.sh
@@ -2,3 +2,6 @@
 
 brew cask install telegram
 brew cask install evernote
+mas install 1481669779 # Evernote Web Clipper (1.0.5)
+mas install 539883307 # LINE (5.24.1)
+mas install 1449412482 # Reeder (4.2.3)
