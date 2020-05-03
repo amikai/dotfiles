c6a56a874a2c18012d0b00c154c340df6330e9bc
莊雲凱 <amikai@zhuangyunkais-MacBook-Pro.local>
2020-05-03 19:16:49 +0800

    Neovim setting installation script

--- tools/neovim.sh
+++ tools/neovim.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 
-git clone https://github.com/amikai/nvimrc "$DOTFILES_DIR"/config/nvim
-nvim +qa
-nvim +checkhealth +"call dein#remote_plugins()" +qa
+download_nvimrc() {
+	git clone https://github.com/amikai/nvimrc "$DOTFILES_DIR"/config/nvim
+}
+
+install_dep() {
+	local PIP2="$(pyenv which pip2)"
+	local PIP3="$(pyenv which pip3)"
+	"$PIP2" install pynvim neovim
+	"$PIP3" install pynvim neovim
+}
+
+install_nvim_plugin() {
+	nvim --cmd "so $DOTFILES_DIR/config/nvim/init.vim" +checkhealth +"call dein#remote_plugins()" +qa
+}
+
+download_nvimrc
+install_dep
+install_nvim_plugin
