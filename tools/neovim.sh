0d36e5c81747462021b73e3e16ec87ea98021654
amikai <as23041248@gmail.com>
2020-05-05 11:25:23 +0800

    Install dependency for neovim plugins

--- tools/neovim.sh
+++ tools/neovim.sh
@@ -10,6 +10,24 @@ install_dep() {
 	local PIP3="$(pyenv which pip3)"
 	"$PIP2" install pynvim neovim
 	"$PIP3" install pynvim neovim
+
+    # For code navigation and plugin dependencies
+    brew install global
+    brwe install cscope
+    brew install universal-ctags/universal-ctags/universal-ctags
+
+    # Shell linter
+    brew install shellcheck
+    # Vim linter
+    "$PIP3" install vim-vint
+    # Python linter
+    "$PIP3" install flake8
+
+    # Python code completion
+    "$PIP3" install jedi
+    # Rust code completion and navigation.
+    cargo install racer
+
 }
 
 install_nvim_plugin() {
