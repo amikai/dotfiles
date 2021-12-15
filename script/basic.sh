23db0386c9fbc1a82698f78ae93c12ef15876a0d
amikai <as23041248@gmail.com>
2021-12-15 15:15:10 +0800

    use zsh as default shell

--- script/basic.sh
+++ script/basic.sh
@@ -28,11 +28,13 @@ brew install gnu-sed
 # Install a modern version of Bash.
 brew install bash
 brew install bash-completion2
+# Install modern zsh
+brew install zsh
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
+if ! fgrep -q "${BREW_PREFIX}/bin/zsh" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/zsh";
 fi;
 
 #
