f1ceacfc64cda6f73cc4844d420a73d6f6921e94
amikai <as23041248@gmail.com>
2021-12-15 15:14:45 +0800

    delete evernote installation

--- script/extra.sh
+++ script/extra.sh
@@ -15,8 +15,6 @@ brew install rustup-init
 brew cask install visual-studio-code
 brew cask install google-chrome
 brew cask install telegram
-brew cask install evernote
-mas install 1481669779 # Evernote Web Clipper (1.0.5)
 mas install 539883307 # LINE (5.24.1)
 mas install 1449412482 # Reeder (4.2.3)
 
