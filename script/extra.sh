57e647b0b8badddef8676317df407f0c88d7e201
amikai <as23041248@gmail.com>
2022-08-19 17:34:00 +0800

    Update cask app

--- script/extra.sh
+++ script/extra.sh
@@ -14,20 +14,19 @@ brew install --cask google-chrome
 brew install --cask iterm2
 brew install --cask docker
 brew install --cask fork
-brew install --cask tableplus
 brew install --cask telegram
 brew install --cask cleanshot
 brew install --cask telegram
 brew install --cask discord
 brew install --cask fork # Git GUI
-brew install --cask cleanshot
-brew install --cask tableplus
+# brew install --cask cleanshot
 brew install --cask logseq
 brew install --cask postman
 brew install --cask welly
 brew install --cask raycast
 mas install 360593530 # Notability
-mas install 1505432629 # Noteplan
+# mas install 1505432629 # Noteplan
 mas install 539883307 # LINE (5.24.1)
 mas install 1449412482 # Reeder (4.2.3)
+mas install 1480068668 # Messenger
 
