066c2969a12c21399b5a12ceb5b420d33ef5cf8d
amikai-chuang <amikai_chuang@trendmicro.com>
2024-10-12 11:09:07 +0800

    fix(fzf): fix the fzf key mapping conflict with zvm

--- config/zsh/zimrc.zsh
+++ config/zsh/zimrc.zsh
@@ -8,8 +8,15 @@ if (( ${+commands[fzf]} )); then
     zmodule fzf
 fi
 
-ZVM_VI_INSERT_ESCAPE_BINDKEY=jk
+ZVM_VI_ESCAPE_BINDKEY=jk
 zmodule jeffreytse/zsh-vi-mode
+
+function zvm_after_init() {
+    zvm_bindkey viins '^R' fzf-history-widget
+    zvm_bindkey vicmd '/' fzf-history-widget
+    zvm_bindkey -M emacs '^R' fzf-history-widget
+}
+
 
 zmodule ohmyzsh/ohmyzsh --root plugins/colored-man-pages
 autoload -U colors && colors
