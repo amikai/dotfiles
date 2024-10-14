cd65847fc55f1a514a401f5e6bc5ceb9d197a47f
amikai-chuang <amikai_chuang@trendmicro.com>
2024-10-14 21:15:24 +0800

    fix the wrong zvm_bindkey

--- config/zsh/zimrc.zsh
+++ config/zsh/zimrc.zsh
@@ -14,7 +14,7 @@ zmodule jeffreytse/zsh-vi-mode
 function zvm_after_init() {
     zvm_bindkey viins '^R' fzf-history-widget
     zvm_bindkey vicmd '/' fzf-history-widget
-    zvm_bindkey -M emacs '^R' fzf-history-widget
+    zvm_bindkey emacs '^R' fzf-history-widget
 }
 
 
