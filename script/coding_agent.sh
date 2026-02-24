d28816ab32c350bf0397c3a3e35e6f9f6b4fc7f4
amikai <amikai.chuang@gmail.com>
2026-02-24 15:48:19 +0800

    install superpower for gemini

--- script/coding_agent.sh
+++ script/coding_agent.sh
@@ -2,7 +2,7 @@
 
 # install superpowers for codex and opencode under ~/.agent folder
 bunx skills add https://github.com/obra/superpowers --global \
-    --agent opencode codex --yes \
+    --agent opencode codex gemini-cli --yes \
     --skill brainstorming dispatching-parallel-agents executing-plans finishing-a-development-branch \
     receiving-code-review requesting-code-review subagent-driven-development systematic-debugging test-driven-development \
     using-git-worktrees using-superpowers verification-before-completion writing-plans writing-skills
