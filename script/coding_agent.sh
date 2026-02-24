540a354f139d8ee469cac5d217994fb83aea8a60
amikai <amikai.chuang@gmail.com>
2026-02-24 15:52:56 +0800

    install all skills for superpowers instead of one by one

--- script/coding_agent.sh
+++ script/coding_agent.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
 # install superpowers for codex and opencode under ~/.agent folder
-bunx skills add https://github.com/obra/superpowers --global \
-    --agent opencode codex gemini-cli --yes \
-    --skill brainstorming dispatching-parallel-agents executing-plans finishing-a-development-branch \
-    receiving-code-review requesting-code-review subagent-driven-development systematic-debugging test-driven-development \
-    using-git-worktrees using-superpowers verification-before-completion writing-plans writing-skills
+bunx skills add https://github.com/obra/superpowers --global --yes \
+    --agent opencode codex gemini-cli
