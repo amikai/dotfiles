432c1cf019d9660b2b36906df4ab154f1df0d32d
amikai <amikai.chuang@gmail.com>
2026-04-08 16:03:39 +0800

    update coding agent installation

--- script/coding_agent.sh
+++ script/coding_agent.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # install superpowers for codex and opencode under ~/.agent folder
+# claude-code use superpowers by plugin
 bunx skills add https://github.com/obra/superpowers --global --yes \
     --agent opencode codex gemini-cli
